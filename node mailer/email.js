92c92679776371e35b2fca9f8b10c43a45228d7a
Lakshay Singal <singallakshay84@gmail.com>
2025-03-20 20:17:05 +0530

    node mailer

--- node mailer/email.js
+++ node mailer/email.js
@@ -4,13 +4,13 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "singallakshay04@gmail.com", 
-    pass: "stur dhij tpjp hupf",   
+    pass: "",   
   },
 });
 
 const mailOptions = {
   from: "your-email@gmail.com",
-  to: "ishan0609.be23@chitkara.edu.in",  
+  to: "ex@gmail.com",  
   subject: "Email test",
   text: "Hello! This is a test email sent using Nodemailer in Node.js. by Lakshay",
 };
